0ee62aba3eeb5c7e37b65229d3796ef5d2af79d0
Gustavo A Ronconi <gustavo.ronconi@olist.com>
2023-12-06 18:42:29 -0300

    brex: btree

--- btree/btree.go
+++ btree/btree.go
@@ -1,17 +1,13 @@
 package btree
 
+// bloomberg interview https://www.youtube.com/watch?v=jma9hFQSCDk&t=78s&ab_channel=KeepOnCoding
+
 type Node struct {
 	key    int
 	left   *Node
 	right  *Node
 	parent *Node
 }
-
-// k = 9
-// 20 < 9 = false
-// 20 > 9 = true
-// n.left == nil = true
-// n.left = Node{key: 9}
 
 func (n *Node) insert(k int) {
 	if n.key < k {
@@ -28,85 +24,7 @@ func (n *Node) insert(k int) {
 		}
 	}
 }
-
-// btree
-// // // 20 //
-// // 9 // 25 //
-// 5 // 12 //
-// // 11 // 14 //
-
-// first recursive
-// root node = 20
-// val = 13
-// diference = -1
-// sucessor = -1
-
-// 20 > 13 = true
-// 9 > 13 = false
-// diference = 7
-// sucessor = 20
-
-// second recursive
-// root node = 12
-// val = 13
-// diference = 7
-// sucesseor = 20
-
-// 12 > 13 = false
-// 14 > 13 = true
-// 14 - 13 = 1
-// 1 < 7 = true
-// diference = 1
-// sucessor = 14
-
-// last recursive
-// root node = 14
-// val = 13
-// diference = 1
-// sucessor = 14
-
-//
-
-// func (n *Node) findOrderSucessor(val int, diference int, sucessor int) int {
-// 	// greater than value - go to the left
-// 	if n.key > val {
-// 		if n.left == nil {
-// 			return sucessor
-// 		}
-// 		if n.left.key > val {
-// 			if ((n.left.key - val) < diference) || sucessor == -1 {
-// 				diference = n.left.key - val
-// 				sucessor = n.left.key
-// 			}
-// 			sucessor = n.left.findOrderSucessor(val, diference, sucessor)
-// 		} else {
-// 			diference = n.key - val
-// 			sucessor = n.key
-// 			sucessor = n.left.right.findOrderSucessor(val, diference, sucessor)
-// 		}
-// 	}
-
-// 	// less than value - go to the right
-// 	if n.key < val {
-// 		if n.right == nil {
-// 			return sucessor
-// 		}
-// 		if n.right.key > val {
-// 			if ((n.right.key - val) < diference) || sucessor == -1 {
-// 				diference = n.right.key - val
-// 				sucessor = n.right.key
-// 			}
-// 			sucessor = n.right.findOrderSucessor(val, diference, sucessor)
-// 		} else {
-// 			diference = n.key - val
-// 			sucessor = n.key
-// 			sucessor = n.left.right.findOrderSucessor(val, diference, sucessor)
-// 		}
-// 	}
-
-// 	return sucessor
-
-// }
+// O(n) complexity and O(1) memory
 
 func (n *Node) findOrderSucessor(val int, diference int, sucessor int) int {
 	if n.key > val {
